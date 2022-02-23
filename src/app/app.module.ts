8756c0db6f55f8af6a1ad13bce8287bc906a8b6f
Oscar Abella <oskr.abella@gmail.com>
2022-02-23 11:40:15 -0500

    updated landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { CsChatBotComponent } from './cs-chat-bot/cs-chat-bot.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CsChatBotComponent
+    CsChatBotComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,

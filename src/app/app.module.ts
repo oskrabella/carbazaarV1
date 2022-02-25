4265895e7f3fa8f19b46f0c1eadf5cb56526ee04
Oscar Abella <oskr.abella@gmail.com>
2022-02-24 20:44:27 -0500

    updated main components, ngx autocomplete and forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GooglePlaceModule } from "ngx-google-places-autocomplete";
 import { AppRoutingModule } from './app-routing.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
@@ -21,6 +25,10 @@ import { ReservationsComponent } from './reservations/reservations.component';
     BrowserModule,
     AppRoutingModule,
     GooglePlaceModule,
+    NgbModule,
+    // NgSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reservations',
@@ -16,20 +17,56 @@ export class ReservationsComponent implements OnInit {
   dropoffTime = '';
   age = '';
   residency = '';
-  constructor() { }
-  @ViewChild("placesRef") placesRef : GooglePlaceDirective;
+
+  hoveredDate: NgbDate | null = null;
+  fromDate: NgbDate | null;
+  toDate: NgbDate | null;
+
+  constructor(private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+   }
+  // @ViewChild("placesRef") placesRef : GooglePlaceDirective;
 
   ngOnInit(): void {
   }
 
-  handleAddressChange(address: Address) {
+  onDateSelection(date: NgbDate) {
+  if (!this.fromDate && !this.toDate) {
+    this.fromDate = date;
+  } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+    this.toDate = date;
+  } else {
+    this.toDate = null;
+    this.fromDate = date;
+  }
+}
+
+isHovered(date: NgbDate) {
+  return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) &&
+      date.before(this.hoveredDate);
+}
+
+  isInside(date: NgbDate) { return this.toDate && date.after(this.fromDate) && date.before(this.toDate); }
+
+  isRange(date: NgbDate) {
+    return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) ||
+        this.isHovered(date);
+  }
+
+  validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+    const parsed = this.formatter.parse(input);
+    return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+  }
+
+  handleAddressChange(address: any) {
       console.log(address)
   }
 
-  onPickUpSelection(e) {
+  onPickUpSelection(e: any) {
       //  Parse out the Google data into something more useable
-      const companyData = e;
-      const formattedAddress = companyData.formatted_address;
+      // const companyData = e;
+      // const formattedAddress = companyData.formatted_address;
 
       // this.company = companyData.name;
       // this.businessName = companyData.name || '';
@@ -43,27 +80,28 @@ export class ReservationsComponent implements OnInit {
       // this.ownershipType = '';
       // this.yearEstablished = '';
 
-      const reservationData = {
-          'pickupLoc': this.pickupLoc,
-          'pickupDate': this.pickupDate,
-          'pickupTime': this.pickupTime,
-          'dropoffLoc': this.dropoffLoc,
-          'dropoffDate': this.dropoffDate,
-          'dropoffTime': this.dropoffTime,
-          'age': this.age,
-          'residency': this.residency
-      };
+      // const reservationData = {
+      //     'pickupLoc': this.pickupLoc,
+      //     'pickupDate': this.pickupDate,
+      //     'pickupTime': this.pickupTime,
+      //     'dropoffLoc': this.dropoffLoc,
+      //     'dropoffDate': this.dropoffDate,
+      //     'dropoffTime': this.dropoffTime,
+      //     'age': this.age,
+      //     'residency': this.residency
+      // };
 
       //  Save our cleaned data
-      localStorage.setItem('companyData', JSON.stringify(businessData));
-
-      this.doDefaults();
+      // localStorage.setItem('companyData', JSON.stringify(businessData));
+      //
+      // this.doDefaults();
   }
 
-  watcher(e) {
-    if (e.target.value.length >= 3) {
-        this.company = 'New Company';
-    }
-}
+  watcher(e: any) {
+    // if (e.target.value.length >= 3) {
+    //     this.company = 'New Company';
+    // }
+    console.log(e);
+  }
 
 }
